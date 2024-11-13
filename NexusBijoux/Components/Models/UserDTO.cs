7df9fd28d690d833b9f7ef72afc4fce1794d8202
DESKTOP-IOH1F3C\EQ01 <Deisy Mariana Velasquez>
2024-11-13 16:05:52 -0500

    Se sube components para crear, editar, usuario

---
+++ NexusBijoux/Components/Models/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace NexusBijoux.Components.Models;
+
+public class UserDTO
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public  string Role { get; set; }
+    public string Alias { get; set; }
+}
